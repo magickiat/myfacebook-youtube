eadc1a65e67b3cb3ff6ae94f6ca7168fde28fefe
Kiattikhun Prathumma <kiattikhun.prathumma@gmail.com>
2018-09-24 00:33:53 +0700

    validate simple register

--- src/main/java/com/magicalcyber/myfacebook/web/controller/RegisterController.java
+++ src/main/java/com/magicalcyber/myfacebook/web/controller/RegisterController.java
@@ -1,10 +1,15 @@
 package com.magicalcyber.myfacebook.web.controller;
  
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
- 
+
 import com.magicalcyber.myfacebook.web.form.RegisterForm;
  
 @Controller
@@ -13,7 +18,8 @@ public class RegisterController {
     private static final Logger log = LoggerFactory.getLogger(RegisterController.class);
  
     @PostMapping("/register")
-    String register(RegisterForm registerForm) {
+    String register(@Valid @ModelAttribute("registerForm") RegisterForm registerForm, BindingResult result,
+            Model model) {
         log.info(registerForm.toString());
         return "index";
     }
